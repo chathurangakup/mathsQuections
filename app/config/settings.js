ac30572a50fff8d7e5ceec0f5cdc8cf06cfd0462
uditha <uditha@intervest.lk>
2023-02-19 15:23:34 +0530

    add redux persist and live url

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,11 +1,11 @@
 export const LANG_LIST_FLAG = 'lngList';
 
-// export const BASE_URL = 'https://apeiscole-beckend.herokuapp.com/';
-export const BASE_URL = 'http://apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com/';
+ export const BASE_URL = 'https://ape-iscole-liveapp.herokuapp.com/';
+//export const BASE_URL = 'http://apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com/';
 
-// export const API_ENDPOINT = 'apeiscole-beckend.herokuapp.com';
+ export const API_ENDPOINT = 'ape-iscole-liveapp.herokuapp.com';
 //dev 
-export const API_ENDPOINT = 'apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com'; //live
+//export const API_ENDPOINT = 'apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com'; //live
 //export const API_ENDPOINT = 'apeiscoledev-env.eba-c4sfrscc.ap-northeast-1.elasticbeanstalk.com'; //dev
 
 export const DEFAULT_PROTOCOL = 'http';
