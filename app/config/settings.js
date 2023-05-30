7d081e8e7da66548683b1c0f4f3314e708a2be73
uditha <uditha@intervest.lk>
2023-05-30 07:39:12 +0530

    added http change and lib update

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,16 +1,16 @@
 export const LANG_LIST_FLAG = 'lngList';
 
- export const BASE_URL = 'https://ape-iscole-liveapp.herokuapp.com/';
+ export const BASE_URL = 'https://ape-iscole-live.herokuapp.com/';
 //  export const BASE_URL = 'https://apeiscole-backend.herokuapp.com/';
 //export const BASE_URL = 'http://apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com/';
 
- export const API_ENDPOINT = 'ape-iscole-liveapp.herokuapp.com'; //live
+ export const API_ENDPOINT = 'ape-iscole-live.herokuapp.com'; //live
 //  export const API_ENDPOINT = 'apeiscole-backend.herokuapp.com'; 
  
 //dev 
 //export const API_ENDPOINT = 'apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com'; //live
 //export const API_ENDPOINT = 'apeiscoledev-env.eba-c4sfrscc.ap-northeast-1.elasticbeanstalk.com'; //dev
 
-export const DEFAULT_PROTOCOL = 'http';
+export const DEFAULT_PROTOCOL = 'https';
 
 export const LOGOUT_IMAGE = 'LOGOUT_IMAGE';
