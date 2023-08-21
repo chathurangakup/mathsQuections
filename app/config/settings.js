dd38ae62aae429b859dfa384448e0155a1480e05
uditha <uditha@intervest.lk>
2023-08-21 07:53:52 +0530

    update target sdk

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,10 +1,11 @@
 export const LANG_LIST_FLAG = 'lngList';
 
- export const BASE_URL = 'https://ape-iscole-live.herokuapp.com/';
+
+ export const BASE_URL = 'https://apeiscole-live.herokuapp.com/';
 //  export const BASE_URL = 'https://apeiscole-backend.herokuapp.com/';
 //export const BASE_URL = 'http://apeiscolelive-env-1.eba-6mze7qqf.ap-northeast-1.elasticbeanstalk.com/';
 
- export const API_ENDPOINT = 'ape-iscole-live.herokuapp.com'; //live
+ export const API_ENDPOINT = 'apeiscole-live.herokuapp.com'; //live
 //  export const API_ENDPOINT = 'apeiscole-backend.herokuapp.com'; 
  
 //dev 
